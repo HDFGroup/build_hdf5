8aaa935b4562f3d81e2b38fc2fd8c09ba1a4561e
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-12-01 10:18:07 -0600

    Build script for downloading and building software needed to install 1.10+ versions of HDF5 from svn or git.

---
+++ preinstall_hdf5.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#installs autoconf, automake, libtool to the current directory (installs in a directory called 'util')
+
+CC=gcc
+
+AUTOCONF_VERS=autoconf-2.69
+AUTOMAKE_VERS=automake-1.15
+LIBTOOL_VERS=libtool-2.4.6
+
+#
+# Should not need to modify any lines below
+#
+INSTALL_DIR=$(pwd)
+
+wget http://ftp.gnu.org/gnu/autoconf/$AUTOCONF_VERS.tar.gz
+tar xvzf $AUTOCONF_VERS.tar.gz
+cd $AUTOCONF_VERS
+./configure --prefix=$INSTALL_DIR/util CC=$CC
+make
+make install
+cd ..
+
+wget http://ftp.gnu.org/gnu/automake/$AUTOMAKE_VERS.tar.gz
+tar xvzf $AUTOMAKE_VERS.tar.gz
+cd $AUTOMAKE_VERS
+./configure --prefix=$INSTALL_DIR/util CC=$CC
+make
+make install
+cd ..
+
+wget http://gnu.mirror.vexxhost.com/libtool/$LIBTOOL_VERS.tar.gz
+tar xvzf $LIBTOOL_VERS.tar.gz
+cd $LIBTOOL_VERS
+./configure --prefix=$INSTALL_DIR/util CC=$CC
+make
+make install
+cd ..
+
+#rm -fr autoconf* automake* libtool* util
+#PATH=$INSTALL_DIR/util/bin:$PATH
+
+NO_COLOR='\033[0m'
+GREEN_COLOR='\033[32;01m'
+RED_COLOR='\033[31;01m'
+
+printf "\n${GREEN_COLOR}   *** YOU NEED TO ADD THE util DIR TO PATH ****\n       In bash: ${RED_COLOR} export PATH=\"${INSTALL_DIR}/util/bin:\$PATH\" ${GREEN_COLOR} ${GREEN_COLOR} \n       In csh: ${RED_COLOR} setenv PATH \"${INSTALL_DIR}/util/bin:\$PATH\" ${NO_COLOR} \n\n"
+
